1a03a70e90243b9d838a7bde2ca7ab1c3239b09d
Marcos Kobuchi <marcoskobuchi@Marcoss-MacBook-Pro.local>
2018-10-10 17:00:09 -0300

    Create and set data source for UITableScene

---
+++ Sources/UI/UITableDataSource.swift
@@ -0,0 +1,68 @@
+//
+//  UITableDataSource.swift
+//  CleanKit
+//
+//  Created by Marcos Kobuchi on 09/10/18.
+//
+
+import UIKit
+
+class UITableDataSource: NSObject, UITableViewDataSource, UITableViewDelegate {
+    struct ViewModelItem {
+        let identifier: String
+        var item: TaggedViewModel
+    }
+    
+    struct SectionItem {
+        var viewModel: SectionViewModel
+        var message: String?
+        var items: [ViewModelItem]
+    }
+    
+    private var actionDelegate: ActionCenterDelegate
+    private lazy var items: [ViewModelItem] = []
+    private lazy var identifiers: [String: String] = [:]
+    
+    init(actionDelegate: ActionCenterDelegate) {
+        self.actionDelegate = actionDelegate
+    }
+    
+    func append(collection: TaggedViewModelCollection) {
+        for item in collection.items {
+            let viewModel = "\(type(of: item))"
+            
+            if let identifier = identifiers[viewModel] {
+                self.items.append(ViewModelItem(identifier: identifier, item: item))
+            } else {
+                assertionFailure("The \(viewModel) view model is not binded")
+            }
+        }
+    }
+    
+    func bind<TCell: UITableSceneCell<TViewModel>, TViewModel: TaggedViewModel>(cell: TCell.Type, to viewModel: TViewModel.Type) {
+        let current = identifiers["\(viewModel)"]
+        
+        precondition(current == nil, "The \(viewModel) is binded for \(current!)")
+        identifiers["\(viewModel)"] = "\(cell)"
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.isEmpty ? 0 : self.items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let item = self.items[safe: indexPath.row] else {
+            return UITableViewCell(frame: .zero)
+        }
+        
+        if var cell = tableView.dequeueReusableCell(withIdentifier: item.identifier) as? UITableSceneCellProtocol {
+            cell.delegate = actionDelegate
+            cell.tag = item.item.tag
+            
+            return cell.prepare(viewModel: item.item)
+        } else {
+            fatalError("The \(item.identifier) cell is not based on UITableSceneCell")
+        }
+    }
+    
+}
--- Sources/UI/UITableScene.swift
+++ Sources/UI/UITableScene.swift
@@ -8,13 +8,14 @@
 import UIKit
 
 open class UITableScene<TPresenter: Presenter<TInteractorProtocol>, TInteractor: Interactor, TInteractorProtocol> : UIScene<TPresenter, TInteractor, TInteractorProtocol> {
+    private var dataSource: UITableDataSource?
     
     @IBOutlet public weak var tableView: UITableView! {
         didSet {
-//            dataSource = UISectionedTableDataSource(actionDelegate: self)
-//
-//            tableView.dataSource = dataSource
-//            tableView.delegate = dataSource
+            dataSource = UITableDataSource(actionDelegate: self)
+            
+            tableView.dataSource = dataSource
+            tableView.delegate = dataSource
             
             tableView.estimatedSectionFooterHeight = 1
             tableView.estimatedSectionHeaderHeight = 1
@@ -24,8 +25,36 @@ open class UITableScene<TPresenter: Presenter<TInteractorProtocol>, TInteractor:
             tableView.sectionHeaderHeight = UITableView.automaticDimension
             tableView.rowHeight = UITableView.automaticDimension
             
-//            setupTable()
+            setupTable()
         }
+    }
+    
+    open override func setup(viewModelCenter: ViewModelCenter) {
+        viewModelCenter.observe(background: true) { [weak self] (collection: TaggedViewModelCollection) in
+            guard let strongSelf = self, let dataSource = strongSelf.dataSource else {
+                return
+            }
+            
+            dataSource.append(collection: collection)
+            
+            DispatchQueue.safeSync {
+                // temporarty only
+                strongSelf.tableView.reloadData()
+            }
+        }
+    }
+    
+    public func bind<TCell: UITableSceneCell<TViewModel>, TViewModel: TaggedViewModel>(cell: TCell.Type, to viewModel: TViewModel.Type) {
+        guard let tableView = tableView, let dataSource = dataSource else {
+            fatalError("Table has not yet been initialized")
+        }
+        
+        tableView.register(UINib(nibName: "\(cell)", bundle: nil), forCellReuseIdentifier: "\(cell)")
+        dataSource.bind(cell: cell, to: viewModel)
+    }
+    
+    open func setupTable() {
+        assertionFailure("You need to implement the method \"setupTable()\" to prepare this table")
     }
     
 }
