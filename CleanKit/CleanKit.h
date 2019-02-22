f7a0c8cb67a38f788bb80072e571331fd96871f4
Marcos Kobuchi <marcos_kobuchi@hotmail.com>
2019-02-22 11:30:35 -0300

    Add support to cocoapods. Create universal framework with release configuration.

---
+++ CleanKit/CleanKit.h
@@ -0,0 +1,18 @@
+//
+//  CleanKit.h
+//  CleanKit
+//
+//  Created by Marcos Kobuchi on 21/02/19.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CleanKit.
+FOUNDATION_EXPORT double CleanKitVersionNumber;
+
+//! Project version string for CleanKit.
+FOUNDATION_EXPORT const unsigned char CleanKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CleanKit/PublicHeader.h>
+
+
